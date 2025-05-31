2e4bfcb2143e6ae968b763645702acffc57bb415
Jeon Duk su <jin5114jj@naver.com>
2025-05-31 15:12:23 +0900

    Spring Start

---
+++ DeokBook/src/main/java/org/example/deokbook/DeokBookApplication.java
@@ -0,0 +1,13 @@
+package org.example.deokbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeokBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeokBookApplication.class, args);
+	}
+
+}
---
+++ DeokBook/src/test/java/org/example/deokbook/DeokBookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.deokbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeokBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
